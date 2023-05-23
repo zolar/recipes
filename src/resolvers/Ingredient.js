edcb029f00592c81b0ed4e2f01fb52f0ff524008
Zola Richards <zola.richards@houseofstylist.com>
2023-05-22 22:44:09 -0700

    Deprecate Ingredient.text field. Use detailedDescription instead.

--- src/resolvers/Ingredient.js
+++ src/resolvers/Ingredient.js
@@ -1,5 +1,9 @@
 module.exports = {
   Ingredient: {
     // Ingredient resolvers go here
+    detailedDescription: (parent) => {
+      console.log(parent);
+      return parent.text;
+    }
   },
 };
